d79720431d7aa865c3b39940cd370dc494e50c0d
Kyllian AUGER <kyllianauger14@gmail.com>
2025-09-22 17:17:09 +0200

    #3 mise à jour des boutons.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,19 +32,19 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.button_connexion_Click = new System.Windows.Forms.Button();
+            this.button_fermer = new System.Windows.Forms.Button();
+            this.button_envoyer = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.button4 = new System.Windows.Forms.Button();
+            this.button_recevoir = new System.Windows.Forms.Button();
             this.label5 = new System.Windows.Forms.Label();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.label6 = new System.Windows.Forms.Label();
             this.button5 = new System.Windows.Forms.Button();
-            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox_recp = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
@@ -79,32 +79,35 @@
             this.label2.TabIndex = 3;
             this.label2.Text = "IpeR";
             // 
-            // button1
+            // button_connexion_Click
             // 
-            this.button1.Location = new System.Drawing.Point(329, 23);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(166, 23);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "Créer Socket et Bind(IPeR)";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button_connexion_Click.Location = new System.Drawing.Point(329, 23);
+            this.button_connexion_Click.Name = "button_connexion_Click";
+            this.button_connexion_Click.Size = new System.Drawing.Size(166, 23);
+            this.button_connexion_Click.TabIndex = 4;
+            this.button_connexion_Click.Text = "Créer Socket et Bind(IPeR)";
+            this.button_connexion_Click.UseVisualStyleBackColor = true;
+            this.button_connexion_Click.Click += new System.EventHandler(this.button_connexion_Click_Click);
             // 
-            // button2
+            // button_fermer
             // 
-            this.button2.Location = new System.Drawing.Point(329, 70);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(165, 23);
-            this.button2.TabIndex = 5;
-            this.button2.Text = "Fermer Close()";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button_fermer.Location = new System.Drawing.Point(329, 70);
+            this.button_fermer.Name = "button_fermer";
+            this.button_fermer.Size = new System.Drawing.Size(165, 23);
+            this.button_fermer.TabIndex = 5;
+            this.button_fermer.Text = "Fermer Close()";
+            this.button_fermer.UseVisualStyleBackColor = true;
+            this.button_fermer.Click += new System.EventHandler(this.button_fermer_Click_Click);
             // 
-            // button3
+            // button_envoyer
             // 
-            this.button3.Location = new System.Drawing.Point(329, 119);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(165, 23);
-            this.button3.TabIndex = 6;
-            this.button3.Text = "Envoyer SendTo(IPeD)";
-            this.button3.UseVisualStyleBackColor = true;
+            this.button_envoyer.Location = new System.Drawing.Point(329, 119);
+            this.button_envoyer.Name = "button_envoyer";
+            this.button_envoyer.Size = new System.Drawing.Size(165, 23);
+            this.button_envoyer.TabIndex = 6;
+            this.button_envoyer.Text = "Envoyer SendTo(IPeD)";
+            this.button_envoyer.UseVisualStyleBackColor = true;
+            this.button_envoyer.Click += new System.EventHandler(this.button_envoyer_Click);
             // 
             // label3
             // 
@@ -138,14 +141,15 @@
             this.label4.TabIndex = 10;
             this.label4.Text = "IPeD";
             // 
-            // button4
+            // button_recevoir
             // 
-            this.button4.Location = new System.Drawing.Point(329, 170);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(165, 35);
-            this.button4.TabIndex = 11;
-            this.button4.Text = "Recevoir ReceiveFrom() Bloquant Timeout";
-            this.button4.UseVisualStyleBackColor = true;
+            this.button_recevoir.Location = new System.Drawing.Point(329, 170);
+            this.button_recevoir.Name = "button_recevoir";
+            this.button_recevoir.Size = new System.Drawing.Size(165, 35);
+            this.button_recevoir.TabIndex = 11;
+            this.button_recevoir.Text = "Recevoir ReceiveFrom() Bloquant Timeout";
+            this.button_recevoir.UseVisualStyleBackColor = true;
+            this.button_recevoir.Click += new System.EventHandler(this.button_recevoir_Click);
             // 
             // label5
             // 
@@ -182,33 +186,33 @@
             this.button5.Text = "CLS";
             this.button5.UseVisualStyleBackColor = true;
             // 
-            // textBox5
+            // textBox_recp
             // 
-            this.textBox5.Location = new System.Drawing.Point(53, 244);
-            this.textBox5.Multiline = true;
-            this.textBox5.Name = "textBox5";
-            this.textBox5.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox5.Size = new System.Drawing.Size(198, 112);
-            this.textBox5.TabIndex = 17;
+            this.textBox_recp.Location = new System.Drawing.Point(53, 244);
+            this.textBox_recp.Multiline = true;
+            this.textBox_recp.Name = "textBox_recp";
+            this.textBox_recp.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox_recp.Size = new System.Drawing.Size(198, 112);
+            this.textBox_recp.TabIndex = 17;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(506, 381);
-            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox_recp);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button_recevoir);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button_envoyer);
+            this.Controls.Add(this.button_fermer);
+            this.Controls.Add(this.button_connexion_Click);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
@@ -226,19 +230,19 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button_connexion_Click;
+        private System.Windows.Forms.Button button_fermer;
+        private System.Windows.Forms.Button button_envoyer;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button_recevoir;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox_recp;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -4,6 +4,8 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -17,35 +19,33 @@ namespace socketUDP
             InitializeComponent();
         }
 
-     
+        private Socket SSockUDP;
         private void button_cls_Click(object sender, EventArgs e)
         {
             this.textBox_recp.Clear();
         }
 
-        private void button_connexion_Click(object sender, EventArgs e)
+        private void button_connexion_Click_Click(object sender, EventArgs e)
         {
-            this.SSockUDP = new Socket(AddressFamily.InterNetwork,
-            SocketType.Stream,
-            ProtocolType.Tcp);
+            this.SSockUDP = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 
             //connection sur le serveur 127.0.0.1 port 80 
             IPEndPoint iped = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 80);
 
-            
+
             try
             {
                 this.SSockUDP.Connect(iped);
             }
-            catch 
+            catch
             {
-                MessageBox.Show("Erreur Connexion"); 
+                MessageBox.Show("Erreur Connexion");
             }
         }
 
-        private void button_fermer_Click(object sender, EventArgs e)
+        private void button_fermer_Click_Click(object sender, EventArgs e)
         {
-            this.SSockUDP.Shutdown(SocketShutdown.Both); 
+            this.SSockUDP.Shutdown(SocketShutdown.Both);
             this.SSockUDP.Close();
         }
 
@@ -54,6 +54,14 @@ namespace socketUDP
             this.SSockUDP.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReceiveTimeout, 5000);
             var messageEnvoi = Encoding.ASCII.GetBytes("Get /\r\n\r\n");
             this.SSockUDP.Send(messageEnvoi);
+        }
+
+        private void button_recevoir_Click(object sender, EventArgs e)
+        {
+            var messageRecu = new byte[1024];
+            int nbcarrecu = this.SSockUDP.Receive(messageRecu);
+            this.textBox_recp.Text = "nbcarecu " + nbcarrecu + "\n" +
+                         Encoding.ASCII.GetString(messageRecu, 0, nbcarrecu);
         }
     }
 }
