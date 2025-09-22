153d9230774b7f8c93a0bda68c080e6d44fede0a
Kyllian AUGER <kyllianauger14@gmail.com>
2025-09-22 16:59:15 +0200

    Revert "commit #3"
    This reverts commit a32115912dd771ecfc8f22ed79f7a866d9dfdc92.

--- Form1.cs
+++ Form1.cs
@@ -4,8 +4,6 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Net;
-using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -14,51 +12,9 @@ namespace socketUDP
 {
     public partial class Form1 : Form
     {
-       // private  Socket socket;
-       private Socket SSockUDP;
         public Form1()
         {
             InitializeComponent();
-        }
-
-     
-        private void button_cls_Click(object sender, EventArgs e)
-        {
-            this.textBox_recp.Clear();
-        }
-
-        private void button_connexion_Click(object sender, EventArgs e)
-        {
-            this.SSockUDP = new Socket(AddressFamily.InterNetwork,
-            SocketType.Stream,
-            ProtocolType.Tcp);
-
-            //connection sur le serveur 127.0.0.1 port 80 
-            IPEndPoint iped = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 80);
-
-            
-
-            try
-            {
-                this.SSockUDP.Connect(iped);
-            }
-            catch 
-            {
-                MessageBox.Show("Erreur Connexion"); 
-            }
-        }
-
-        private void button_fermer_Click(object sender, EventArgs e)
-        {
-            this.SSockUDP.Shutdown(SocketShutdown.Both); 
-            this.SSockUDP.Close();
-        }
-
-        private void button_envoyer_Click(object sender, EventArgs e)
-        {
-            this.SSockUDP.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReceiveTimeout, 5000);
-            var messageEnvoi = Encoding.ASCII.GetBytes("Get /\r\n\r\n");
-            this.SSockUDP.Send(messageEnvoi);
         }
     }
 }
